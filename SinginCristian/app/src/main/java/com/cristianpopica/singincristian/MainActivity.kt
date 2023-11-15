4aad2522b0d750d02717121e65988533a2b79346
cristian9761 <cristian975112@gmail.com>
2023-11-15 23:13:34 +0100

    Funcionalidades formulario ejercicio singin

--- SinginCristian/app/src/main/java/com/cristianpopica/singincristian/MainActivity.kt
+++ SinginCristian/app/src/main/java/com/cristianpopica/singincristian/MainActivity.kt
@@ -3,11 +3,13 @@ package com.cristianpopica.singincristian
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.cristianpopica.singincristian.ui.RegisterForm
+import com.cristianpopica.singincristian.ui.Screens.appContent
+import com.cristianpopica.singincristian.ui.Screens.profileInfo
 import com.cristianpopica.singincristian.ui.theme.SinginCristianTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +22,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RegisterForm()
+                   Column(
+                       modifier = Modifier.fillMaxSize()
+                   ) {
+                       profileInfo(profileName = "Cristian Popica", profilePictureDrawableId = R.drawable.profilepicture)
+                       appContent()
+                   }
                 }
             }
         }
