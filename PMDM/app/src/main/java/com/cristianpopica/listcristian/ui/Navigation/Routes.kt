2e65c4b1f1ab6dc4f5743339fd563895f7e2808e
Cristian <105856280+cristian9751@users.noreply.github.com>
2023-12-10 16:58:17 +0100

    Delete PMDM directory

--- PMDM/app/src/main/java/com/cristianpopica/listcristian/ui/Navigation/Routes.kt
+++
@@ -1,6 +0,0 @@
-package com.cristianpopica.listcristian.ui.Navigation
-
-sealed class Routes(val route : String) {
-    object MainScreen : Routes("main_screen")
-    object MovieInfo : Routes("movie_info_screen")
-}
